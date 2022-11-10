cab27f5504b83dc79edf2a4e2d2f09027d2da1c5
Josh Dales <josh@akira.md>
2022-11-10 09:56:02 -0500

    Add helper functions for checking limits

--- src/enforcer.ts
+++ src/enforcer.ts
@@ -3,8 +3,8 @@ import { Client } from './client'
 import * as core from '@actions/core'
 
 export interface Limits {
-  repoLimit: number,
-  perAuthorLimit: number
+  repoLimit?: number,
+  perAuthorLimit?: number
 }
 
 export class Enforcer {
@@ -27,16 +27,47 @@ export class Enforcer {
     const triggeringPR: PullRequest | undefined = openPRs.find(pr => pr.number === this.triggeringPrNumber)
 
     if (triggeringPR) {
-      const openPRsForAuthor = openPRs.filter(pr => pr.author === triggeringPR.author)
-
-      core.debug(`Current open PRs in the repos is ${openPRs.length}`);
-      core.debug(`Current open PRs for ${triggeringPR.author} is ${openPRsForAuthor.length}`);
-
-      if (openPRs.length > this.limits.repoLimit || openPRsForAuthor.length > this.limits.perAuthorLimit){
+      if (this.closeBasedOnRepoLimit(openPRs)) {
         await this.client.closePullRequest(triggeringPR, "Sorry, this pull request will be closed. The limit for open pull requests was exceeded.")
+      }
+
+      if (this.closeBasedOnAuthorLimit(openPRs, triggeringPR)) {
+        await this.client.closePullRequest(triggeringPR, "Sorry, this pull request will be closed. You have too many open PRs.")
       }
     } else {
       core.info("The triggering PR is closed, no action will be taken.");
     }
+  }
+
+  closeBasedOnRepoLimit(openPrs: PullRequest[]): boolean {
+    if (!this.limits.repoLimit) {
+      core.debug(`There is no repo PR limit set`)
+      return false
+    }
+
+    core.debug(`Current number of open PRs in the repos is ${openPrs.length}`)
+    if (this.limits.repoLimit > openPrs.length) {
+      return false
+    }
+
+    core.debug(`There are more PRs open in this repo than the limit allows`)
+    return true
+  }
+
+  closeBasedOnAuthorLimit(openPRs: PullRequest[], triggeringPR: PullRequest): boolean {
+    if (!this.limits.perAuthorLimit) {
+      core.debug(`There is no author PR limit set`)
+      return false
+    }
+
+    const openPRsForAuthor = openPRs.filter(pr => pr.author === triggeringPR.author)
+    core.debug(`Current number of open PRs for ${triggeringPR.author} is ${openPRsForAuthor.length}`)
+
+    if (this.limits.perAuthorLimit > openPRsForAuthor.length) {
+      return false
+    }
+
+    core.debug(`The author of this PRs has more PRs open than the limit allows`)
+    return true
   }
 }
